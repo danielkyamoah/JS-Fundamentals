2f86bc91efc35f526bbdb77906803bd03697e47a
Daniel Kofi Yamoah <dnnkof@gmail.com>
2025-09-12 20:50:42 +0000

    added a new file and modified it

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+// Grab all arguments after the first two default ones
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
